150870a99b6652099cb65ea4656fd5029ff09a52
DanielMojonero <dmojoneromoya@iesesteveterradas.cat>
2017-06-12 20:11:28 +0200

    tutorial actualizado

--- mysite/keysexample.py
+++ mysite/keysexample.py
@@ -2,5 +2,5 @@
 # copiar el contenido de 'keysexample.py' en 'keys.py' pero rellenando
 # las variables con los credenciales que te otorgue google
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ''
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '123456789098-gd73nd7f3nfosyfje7mfindnsb3nepmg9enwsdm3r.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'BVY7Vb7VCy-FFDF65f7_df6fFU'
