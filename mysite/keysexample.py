7c8f13d8928909ce5fbb5d34f30ba8bbada2e3fa
DanielMojonero <dmojoneromoya@iesesteveterradas.cat>
2017-06-12 20:20:49 +0200

    especificacion

--- mysite/keysexample.py
+++ mysite/keysexample.py
@@ -1,6 +1,7 @@
 # A partir de este archivos tienes que crear un archivo llamado 'keys.py' y
 # copiar el contenido de 'keysexample.py' en 'keys.py' pero rellenando
-# las variables con los credenciales que te otorgue google
+# las variables con los credenciales que te otorgue google, si has seguido
+# el tutorial de deployment es posible que este ya sea tu archivo 'keys.py'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '123456789098-gd73nd7f3nfosyfje7mfindnsb3nepmg9enwsdm3r.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'BVY7Vb7VCy-FFDF65f7_df6fFU'
